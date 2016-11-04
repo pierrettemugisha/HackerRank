75df49c01c4712d1badd8590669c69e9f1e0ca82
Pierrette Mugisha <Pierrette Mugisha>
2016-11-03 22:42:38 -0300

    Oct-Nov 2016: Day 24

---
+++ src/Day24_MoreLinkedLists/Node.java
@@ -0,0 +1,13 @@
+package Day24_MoreLinkedLists;
+
+/**
+ * Created by PierretteMugisha on 2016-11-01.
+ */
+public class Node {
+    int data;
+    Node next;
+    Node(int d){
+        data=d;
+        next=null;
+    }
+}
